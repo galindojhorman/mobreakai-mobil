fe166fff9283221910d1ed4ab9eae52e8f6fd10d
JhormanGalindo <j.galindop@uniandes.edu.co>
2025-03-06 20:06:52 -0500

    feat: 🎸 add banner color and set Poppins as the font family in theme

--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -6,4 +6,5 @@ class AppColors {
   static const Color neutral = Color(0xFFCACACA);
   static const Color mainText = Color(0xFF00747C);
   static const Color alternate = Color(0xFF878787);
+  static const Color banner = Color(0xFF9AF0F9);
 }
