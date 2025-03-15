e1ed2f90723ebbb3e69bc672df562aa33422459b
leinerbarrios <lj.barrios@uniandes.edu.co>
2025-03-14 22:50:37 -0500

    feat: 🎸 routes in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:mobreakai/screens/register.dart';
 import 'screens/active_breaks.dart';
+import 'screens/active_breaks_void.dart';
 import 'screens/create_break.dart';
+import 'screens/create_break_2.dart';
 import 'screens/login.dart';
+import 'screens/register.dart';
 import 'utils/theme.dart';
 
 void main() {
@@ -16,14 +18,15 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: appTheme, 
-      initialRoute: "/create-break",
+      theme: appTheme,
+      initialRoute: "/login",
       routes: {
         "/login": (context) => const LoginScreen(),
         "/active-breaks-list": (context) => const ActiveBreaksListScreen(),
-        "/create-break": (context) => const ActiveBreaksConfigScreen(),
+        "/create-break-1": (context) => const ActiveBreaksConfigScreen1(),
+        "/create-break-2": (context) => const ActiveBreaksConfigScreen2(),
         "/register": (context) => const RegisterScreen(),
-        // Exponer las demás rutas aquí
+        "/active-breaks-void": (context) => const ActiveBreaksVoidScreen(),
       },
     );
   }
